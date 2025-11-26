c7fda160c489659adcc183e81f34971218fda7eb
Bella Ciao <tonistork153@gmail.com>
2025-11-26 20:57:33 +0530

    Inital commit

---
+++ src/phase1/level1/PositiveNegativeZero.java
@@ -0,0 +1,14 @@
+package phase1.level1;
+
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        int num=-1;
+
+        if(num>0)
+            System.out.println("positive");
+        else if(num<0)
+            System.out.println("negative");
+        else
+            System.out.println("zero");
+    }
+}
